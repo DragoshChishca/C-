1d62fd59916f31186abcfc94a6a3cba9768619e9
DragoshChishca <107793412+DragoshChishca@users.noreply.github.com>
2022-06-29 20:15:29 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,49 +1,15 @@
 #include <iostream>
-#include <iomanip>
-#include <conio.h>
-
 using namespace std;
-char a;
-
 int main()
 {
-    cout << "1) Date de intrare: ";
-    a = getche ();
-    cout << "\n";
-    cout << "Date de iesire:\n";
-    cout << setw(1) << setfill (a) << a << endl;
-    cout << setw(2) << setfill (a) << a << endl;
-    cout << setw(3) << setfill (a) << a << endl;
-    cout << setw(4) << setfill (a) << a << endl;
-
-
-    cout << "2) Date de intrare: ";
-    a = getche ();
-    cout << "\n";
-    cout << "Date de iesire:\n";
-    cout << a << " " << a << " " << a << " " << a << endl;
-    cout << " " << a << " " << a << " " << a << endl;
-    cout << "  " << a << " " << a << endl;
-    cout << "   " << a << endl;
-
-    cout << "3) Date de intrare: ";
-    a = getche ();
-    cout << "\n";
-    cout << "Date de iesire:\n";
-    cout << a << "" << a << "" << a << "" << a << endl;
-    cout << " " << a << "" << a << "" << a << endl;
-    cout << "  " << a << "" << a << endl;
-    cout << "   " << a << endl;
-
-    cout << "4) Date de intrare: ";
-    a = getche ();
-    cout << "\n";
-    cout << "Date de iesire:\n";
-    cout << "   " << a << endl;
-    cout << "  " << a << " " << a << endl;
-    cout << " " << a << " " << a << " " << a << endl;
-    cout << a << " " << a << " " << a << " " << a << endl;
-
-
+    int copil_1, copil_2, mere;
+    cout << "Introduceti numarul de mere:" << endl;
+    cin >> mere;
+    copil_1 = mere;
+    copil_2 = mere;
+    copil_1 = mere - 2;
+    copil_2 = mere + 1;
+    cout << copil_1 << endl << copil_2;
     return 0;
 }
+
